5add9023dbd137a1e5cf4ec34d74c18e97dfb79d
Terrance <TerranceBryant165@gmail.com>
2023-03-14 17:28:56 +0300

    My First Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () =>{
+    isNavbarExpanded = !isNavbarExpanded;
+    navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+
+
+
+
+
+
+
+//light and dark mode
+const a = window.matchMedia("(prefers-color-scheme: dark)");
+console.log(a.matches);
